d94f15e2b1c524f48956c7d2234d0d50d5201a06
Noemi <nasladieg@gmail.com>
2024-10-04 16:19:05 +0200

    In: #1 Create a new class Nurse and create a new class NurseController. In the NurseController created a new method login.

---
+++ src/main/java/com/example/Hospital/Hospital/Nurse.java
@@ -0,0 +1,55 @@
+package com.example.Hospital.Hospital;
+
+public class Nurse {
+	private int idNurse;
+	private String name;
+	private String password;
+	private int age;
+	private String speciality;
+	private static int totalNurse;
+
+	public Nurse(String name, String password, int age, String speciality) {
+		this.idNurse = totalNurse + 1;
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		this.speciality = speciality;
+	}
+
+	public void setSpeciality(String speciality) {
+		this.speciality = speciality;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSpeciality() {
+		return speciality;
+	}
+
+	public int getIdNurse() {
+		return idNurse;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
