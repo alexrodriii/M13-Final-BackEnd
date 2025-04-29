5945dd8a02b60d34618196caa654c087c0c5411a
alexrodriii <alexcp2004@gmail.com>
2025-04-29 16:10:00 +0200

    Fixed #16 patient table

--- src/main/java/com/example/Hospital/Hospital/Patient.java
+++ src/main/java/com/example/Hospital/Hospital/Patient.java
@@ -6,49 +6,44 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity(name="Pacientes")
+@Entity(name = "Pacientes")
 public class Patient {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="Patient_id")
-	private String id;
+	@Column(name = "Patient_id")
+	private Integer id;
 
-	@Column(name="Patient_Name")
+	@Column(name = "Patient_Name")
 	private String name;
-	
-	public Patient() {}
-	
 
-	public Patient(String id, String name) {
-		this.id = id;
-		this.name = name;
-		
+	public Patient() {
 	}
 
+	public Patient(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+
+	}
 
 	@Override
 	public String toString() {
 		return "Patient [id=" + id + ", name=" + name + "]";
 	}
 
-
-	public String getId() {
+	public Integer getId() {
 		return id;
 	}
 
-
-	public void setId(String id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
-	} 
-	
+	}
+
 }
--- src/main/java/com/example/Hospital/Hospital/PatientRepository.java
+++ src/main/java/com/example/Hospital/Hospital/PatientRepository.java
@@ -2,6 +2,6 @@ package com.example.Hospital.Hospital;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface PatientRepository extends CrudRepository<Patient, String>{
+public interface PatientRepository extends CrudRepository<Patient, Integer>{
 
 }
